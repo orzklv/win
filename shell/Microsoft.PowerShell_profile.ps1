85f9d279b5203ce73aafa734bb68b6f7d7b6c8e0
Yūri Katsuki <yuri@katsuki.moe>
2023-01-20 10:47:34 +0500

    adding windows powershell dotfile

---
+++ shell/Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,22 @@
+# Staring up starship custom prompt
+Invoke-Expression (&starship init powershell)
+
+# Initialize volta
+(& volta completions powershell) | Out-String | Invoke-Expression
+
+# Alias ls 
+Remove-Item alias:ls -Force
+Remove-Item alias:sl -Force
+New-Alias -Name ls -Value lsd
+New-Alias -Name sl -Value lsd
+New-Alias -Name celar -Value clear
+New-Alias -Name j -Value just.exe
+New-Alias -Name open -Value explorer.exe
+New-Alias -Name top -Value btop
+New-Alias -Name vim -Value helix
+
+# Functions
+# function cxsmxs {
+#     ssh cxsmxs
+# }
+
