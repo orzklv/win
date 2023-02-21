153fc204b63fd83f4fca2777409ed4376ebd98c7
Yūri Katsuki <yuri@katsuki.moe>
2023-02-21 13:11:54 +0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,3 +6,22 @@ ln -sf ~/.dots/shells/.zshrc ~/.zshrc
 
 # Install helix configs
 ln -sf ~/.dots/.config/helix ~/.config
+
+# If it's linux, not macos
+if [ "$(uname)" == "Linux" ]; then
+    # Install bspwm config
+    ln -sf ~/.dots/.config/bspwm ~/.config
+
+    # Install sxhkd config
+    ln -sf ~/.dots/.config/sxhkd ~/.config
+
+    # Install picom config
+    ln -sf ~/.dots/.config/picom.conf ~/.config
+
+    # Install paru config
+    ln -sf ~/.dots/.config/paru ~/.config
+
+    # Install alacritty config
+    mkdir -p ~/.config/alacritty
+    ln -sf ~/.dots/terms/Alacritty/alacritty.yml ~/.config/alacritty/
+fi
