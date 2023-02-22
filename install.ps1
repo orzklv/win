d3c85be7745b8e6d6a2c023b94d642d190549330
Yūri Katsuki <yuri@katsuki.moe>
2023-02-22 09:52:33 +0500

    completed ps1 installation instructions

--- install.ps1
+++ install.ps1
@@ -28,24 +28,32 @@ ForEach ($App in $CargoApps) {
     cargo install $App
 }
 
+# Create a function that checks if file or folder on param exists, if exists delete it
+Function Remove-FileIfExists($Path) {
+    If (Test-Path -Path $Path) {
+        Remove-Item -Path $Path -Force -Recurse
+    }
+}
+
 # Check if the folder AppData\Roaming\lsd and config.yaml exists, if not create it and create symlink to the config.yaml file at APPDATA\.config\lsd\config.yaml:
 If (!(Test-Path -Path $Env:APPDATA\lsd)) {
+    Remove-FileIfExists($Env:APPDATA\lsd)
     New-Item -ItemType SymbolicLink -Path $Env:APPDATA\lsd -Target $Env:APPDATA\.dots\.config\lsd
 }
 
 # Check if topgrade.toml exists at AppData\Roaming, if not copy the file at ./config/windows.topgrade.toml as topgrade.toml:
 If (!(Test-Path -Path $Env:APPDATA\topgrade.toml)) {
+    Remove-FileIfExists($Env:APPDATA\topgrade.toml)
     New-Item -ItemType SymbolicLink -Path $Env:APPDATA\topgrade.toml -Target $Env:APPDATA\.dots\.config\windows.topgrade.toml
 }
 
 # Move ./shells/Microsoft.PowerShell_profile.ps1 to $PROFILE
-New-Item -ItemType SymbolicLink -Path $Env:APPDATA\lsd -Target $Env:APPDATA\.dots\.config\lsd
-# TODO: make a check whether is Document folder synced with OneDrive
-Move-Item -Path $Env:APPDATA\.dots\shells\Microsoft.PowerShell_profile.ps1 -Destination $PROFILE
+Remove-FileIfExists($PROFILE)
+New-Item -ItemType SymbolicLink -Path $PROFILE -Target $Env:APPDATA\.dots\shells\Microsoft.PowerShell_profile.ps1
 
 # Override ./terms/Windows Terminal/settings.json with the one at AppData\Local\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\settings.json
-New-Item -ItemType SymbolicLink -Path $Env:APPDATA\lsd -Target $Env:APPDATA\.dots\.config\lsd
-Copy-Item -Path $Env:APPDATA\.dots\terms\Windows Terminal\settings.json -Destination $Env:LOCALAPPDATA\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\settings.json -Force
+Remove-FileIfExists($Env:LOCALAPPDATA\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\settings.json)
+New-Item -ItemType SymbolicLink -Path $Env:LOCALAPPDATA\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\settings.json -Target $Env:APPDATA\.dots\terms\Windows Terminal\settings.json
 
 # Done
 Write-Output "The installation has been successfully completed!"
