aba75f72d1919564c69dc2fe52d40c68a3009fe0
Yūri Katsuki <yuri@katsuki.moe>
2023-05-11 10:34:21 +0500

    correct version of windows powershell 5.1

--- install.ps1
+++ install.ps1
@@ -2,7 +2,7 @@
 winget install Git.Git
 
 # Clone a repository from GitHub to ~/.dots
-git clone https://github.com/katsuki-yuri/dots.git $Env:APPDATA\.dots
+git clone https://github.com/katsuki-yuri/dots.git "$Env:APPDATA\.dots"
 
 # Install all required applications using winget
 $Apps = @(
@@ -35,25 +35,31 @@ Function Remove-FileIfExists($Path) {
     }
 }
 
-# Check if the folder AppData\Roaming\lsd and config.yaml exists, if not create it and create symlink to the config.yaml file at APPDATA\.config\lsd\config.yaml:
-If (!(Test-Path -Path $Env:APPDATA\lsd)) {
-    Remove-FileIfExists($Env:APPDATA\lsd)
-    New-Item -ItemType SymbolicLink -Path $Env:APPDATA\lsd -Target $Env:APPDATA\.dots\.config\lsd
+# Check if the folder AppData\Roaming\lsd exists, if not create it and create symlink to the config.yaml file at APPDATA\.dots\.config\lsd
+If (!(Test-Path -Path "$Env:APPDATA\lsd")) {
+    New-Item -ItemType Directory -Path "$Env:APPDATA\lsd"
 }
+New-Item -ItemType SymbolicLink -Path "$Env:APPDATA\lsd\config.yaml" -Target "$Env:APPDATA\.dots\.config\lsd\config.yaml"
 
 # Check if topgrade.toml exists at AppData\Roaming, if not copy the file at ./config/windows.topgrade.toml as topgrade.toml:
-If (!(Test-Path -Path $Env:APPDATA\topgrade.toml)) {
-    Remove-FileIfExists($Env:APPDATA\topgrade.toml)
-    New-Item -ItemType SymbolicLink -Path $Env:APPDATA\topgrade.toml -Target $Env:APPDATA\.dots\.config\windows.topgrade.toml
+If (!(Test-Path -Path "$Env:APPDATA\topgrade.toml")) {
+    New-Item -ItemType SymbolicLink -Path "$Env:APPDATA\topgrade.toml" -Target "$Env:APPDATA\.dots\.config\windows.topgrade.toml"
 }
 
 # Move ./shells/Microsoft.PowerShell_profile.ps1 to $PROFILE
 Remove-FileIfExists($PROFILE)
-New-Item -ItemType SymbolicLink -Path $PROFILE -Target $Env:APPDATA\.dots\shells\Microsoft.PowerShell_profile.ps1
+
+# Make sure if WindowsPowershell folder exists at Documents
+If (!(Test-Path -Path "$Env:USERPROFILE\Documents\WindowsPowerShell")) {
+    New-Item -ItemType Directory -Path "$Env:USERPROFILE\Documents\WindowsPowerShell"
+}
+
+# Create a symlink to the Microsoft.PowerShell_profile.ps1 file at APPDATA\.dots\shells\Microsoft.PowerShell_profile.ps1
+New-Item -ItemType SymbolicLink -Path $PROFILE -Target "$Env:APPDATA\.dots\shells\Microsoft.PowerShell_profile.ps1"
 
 # Override ./terms/Windows Terminal/settings.json with the one at AppData\Local\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\settings.json
-Remove-FileIfExists($Env:LOCALAPPDATA\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\settings.json)
-New-Item -ItemType SymbolicLink -Path $Env:LOCALAPPDATA\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\settings.json -Target $Env:APPDATA\.dots\terms\Windows Terminal\settings.json
+Remove-FileIfExists("$Env:LOCALAPPDATA\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\settings.json")
+New-Item -ItemType SymbolicLink -Path "$Env:LOCALAPPDATA\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\settings.json" -Target "$Env:APPDATA\.dots\terms\Windows Terminal\settings.json"
 
 # Done
 Write-Output "The installation has been successfully completed!"
