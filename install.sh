27b3a690a7d46a6c51dbaf0f8b9d071e4d9d54b2
Yūri Katsuki <yuri@katsuki.moe>
2023-06-08 18:56:34 +0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -10,6 +10,19 @@ ln -sf ~/.dots/shells/.zshrc ~/.zshrc
 # Install helix configs
 ln -sf ~/.dots/.config/helix ~/.config/helix
 
+# If zsh is not default shell, change it to zsh on mac and linux\
+if [ "$(uname)" == "Linux" ]; then
+  if [ "$SHELL" != "/usr/bin/zsh" ]; then
+    chsh -s /usr/bin/zsh
+  fi
+fi
+
+if [ "$(uname)" == "Darwin" ]; then
+  if [ "$SHELL" != "/bin/zsh" ]; then
+    chsh -s /bin/zsh
+  fi
+fi
+
 # If it's arch linux, install paru aur helper
 if [ "$(uname)" == "Linux" ]; then
 	if [ -f /etc/arch-release ]; then
@@ -83,4 +96,61 @@ if [ "$(uname)" == "Linux" ]; then
 	# Install alacritty config
 	mkdir -p ~/.config/alacritty
 	ln -sf ~/.dots/terms/Alacritty/alacritty.yml ~/.config/alacritty/
+fi
+
+# If it's macos
+if [ "$(uname)" == "Darwin" ]; then
+  # Install brew
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+  # Install rust & rustup
+  curl https://sh.rustup.rs -sSf | sh -s -- -y
+  source "$HOME/.cargo/env"
+
+  # Install deno
+  curl -fsSL https://deno.land/install.sh | sh
+  export DENO_INSTALL="/home/codespace/.deno"
+  export PATH="$DENO_INSTALL/bin:$PATH"
+
+  # Install volta
+  curl https://get.volta.sh | bash
+  export VOLTA_HOME="$HOME/.volta"
+  export PATH="$VOLTA_HOME/bin:$PATH"
+  volta install node@lts pnpm prettier
+
+  # Install brew packages
+  brew_packages=(
+    "btop"
+    "bat"
+    "exa"
+    "procs"
+    "ripgrep"
+    "fd"
+    "gping"
+    "hyperfine"
+    "just"
+    "helix"
+    "starship"
+    "tealdeer"
+    "topgrade-bin"
+    "volta-bin"
+  )
+
+  brew install "${brew_packages[@]}"
+
+  # Install bspwm config
+  ln -sf ~/.dots/.config/bspwm ~/.config/bspwm
+
+  # Install sxhkd config
+  ln -sf ~/.dots/.config/sxhkd ~/.config/sxhkd
+
+  # Install picom config
+  ln -sf ~/.dots/.config/picom.conf ~/.config
+
+  # Install paru config
+  ln -sf ~/.dots/.config/paru ~/.config/paru
+
+  # Install alacritty config
+  mkdir -p ~/.config/alacritty
+  ln -sf ~/.dots/terms/Alacritty/alacritty.yml ~/.config/alacritty/
 fi
